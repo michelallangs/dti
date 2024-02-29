47044c159d70a180c502f546832fdbc741faba2d
Michel Allan Gonçalves <michel.allan@sjc.sp.gov.br>
2024-02-29 19:17:07 -0300

    sticky header

---
+++ app/assets/javascripts/sticky_header.js
@@ -0,0 +1,29 @@
+var ready;
+
+ready = function() {
+  $(".main-container").on("scroll", function() {
+    pinHeader();
+  });
+
+  var header = $(".container-header");
+  var headerH = header.outerHeight();
+  var body = $(".container-body");
+  var parentWidth = header.width();
+  var sticky = header.offset().top;
+  console.log(parentWidth);
+
+  function pinHeader() {
+    if ($(".main-container").scrollTop() > sticky) {
+      header.addClass("sticky");
+      header.width(parentWidth);
+      body.css("padding-top", headerH);
+    } else {
+      header.removeClass("sticky");
+      header.removeAttr("style");
+      body.removeAttr("style");
+    }
+  }
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
